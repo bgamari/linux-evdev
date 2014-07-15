ec7b54a5c9c87e1485bc35215f21e3eb2071a88a
Ben Gamari <bgamari.foss@gmail.com>
2014-07-15 12:52:20 -0400

    Add Device module

--- EventTest.hs
+++ EventTest.hs
@@ -1,11 +1,13 @@
 module Main where
 
 import System.IO
+import System.Linux.Input.Device
 import System.Linux.Input.Event
-import Foreign.Storable
 import Control.Monad
 
-main =
-  do f <- openFile "/dev/input/event4" ReadMode
-     forever $ do a <- hReadEvent f
-                  print a
+main = do
+    f <- openFile "/dev/input/event4" ReadMode
+    getDeviceName f >>= print
+    getDeviceId f >>= print
+    forever $ do a <- hReadEvent f
+                 print a
